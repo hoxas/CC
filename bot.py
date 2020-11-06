f4f40341a3424c514b3fe1bde10ffa7acb57cf94
hoxas <hoxas@live.com>
2020-11-06 01:53:13 -0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,80 @@
+import discord
+from discord.ext import commands
+import managerv2
+
+client = commands.Bot(command_prefix = '')
+
+@client.event
+async def on_ready():
+    print('Bot is ready!')
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined a server.')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left the server.')
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        return await ctx.send("Comando não encontrado. Para obter os comandos digite 'c'")
+    raise error
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+@client.command(aliases=['c'])
+async def comandos(ctx):
+    await ctx.send("""Comandos:
+    'ajuda' ou '?' - Exemplo de input
+    'itens' - Lista de Itens
+    'pedido' ou 'p' (seguido do pedido) - Anotar pedido
+    'tabela' ou 't' - Mostra tabela em string no chat (Não formatada!!!)
+    'excel' ou 'xl' - Exporta o banco de dados para excel
+    'csv' - Exporta o banco de dados para csv
+    'comandos' ou 'c' - Lista de Comandos""")
+
+@client.command(aliases=['?'])
+async def ajuda(ctx):
+    await ctx.send("""Nome, Instagram, Grupo(J, A, I), Sexo(M, F), Valor, Data Pedido, Data Entrega, Pedido(Quantidade-Item1;Quantidade-Item2;...)\n
+Obs Pedido: Não usar ; no final se for apenas 1 item independente de quantidade""")
+
+@client.command()
+async def itens(ctx):
+    await ctx.send("""Itens:
+    Brigadeiro 4
+    Brigadeiro 6
+    Brigadeiro NN 4
+    Brigadeiro NN 6
+    Brownie G
+    Brownie B
+    Cento Brigadeiro
+    Cento Beijinho
+    Panettone""")
+
+@client.command(aliases=['p'])
+async def pedido(ctx, *, nota):
+    result = managerv2.mainfunction(nota)
+    await ctx.send(result)
+
+@client.command(aliases=['t'])
+async def tabela(ctx):
+    result = managerv2.show()
+    await ctx.send(result)
+
+@client.command(aliases=['xl'])
+async def excel(ctx):
+    result = managerv2.excel()
+    await ctx.send(result)
+
+@client.command()
+async def csv(ctx):
+    result = managerv2.csv()
+    await ctx.send(result)
+
+
+
+client.run('Nzc0MDQ0NzI4MDU5NjI1NTAz.X6SDVA.viTpoOC1aATKMsMWNRVU4DXESoU')
---
+++ manager.py
@@ -0,0 +1,120 @@
+import sqlite3, datetime, numpy, openpyxl, pandas as pd
+
+year = datetime.datetime.now().year
+
+connection = sqlite3.connect('cacao.db')
+
+cursor = connection.cursor()
+
+def mainfunction ():
+    # input do usuário
+    inp = input('Nome, Instagram, Grupo(J, A, I), Sexo(M, F), Valor, Data Pedido, Data Entrega, Pedido(Quantidade-Item1;Quantidade-Item2;...): \n')
+    
+    # exit
+    if inp == 'e':
+        print('Exiting & Closing Connection')
+        return connection.close()
+
+    # joins tables and saves to csv
+    if inp == 'csv':
+        df = pd.read_sql("""SELECT pedidos.*, itensPedidos.'Brigadeiro 4', itenspedidos.'Brigadeiro 6', itensPedidos.'Brigadeiro NN 4', itensPedidos.'Brigadeiro NN 6', itensPedidos.'Brownie G', itensPedidos.'Brownie B', itensPedidos.'Cento Brigadeiro', itensPedidos.'Cento Beijinho', itensPedidos.'Panettone' 
+            FROM pedidos
+            INNER JOIN itensPedidos ON pedidos.id = itensPedidos.id;
+            """, connection)
+        df.to_csv('pedidos.csv', index=False)
+        return mainfunction()
+        
+    if inp == 'xl':
+        df = pd.read_sql("""SELECT pedidos.*, itensPedidos.'Brigadeiro 4', itenspedidos.'Brigadeiro 6', itensPedidos.'Brigadeiro NN 4', itensPedidos.'Brigadeiro NN 6', itensPedidos.'Brownie G', itensPedidos.'Brownie B', itensPedidos.'Cento Brigadeiro', itensPedidos.'Cento Beijinho', itensPedidos.'Panettone' 
+            FROM pedidos
+            INNER JOIN itensPedidos ON pedidos.id = itensPedidos.id;
+            """, connection)
+        df.to_excel('pedidos.xlsx', sheet_name='Pedidos', index=False)
+        return mainfunction()
+        
+        
+    
+    # separando input pelas virgulas
+    inplist = inp.split(',')
+    i = 0
+    while i < len(inplist) - 1:
+        inplist[i] = inplist[i].strip()
+        i += 1
+
+    # erro se lista < 8 itens
+    if len(inplist) < 8:
+        print('Faltam argumentos!')
+        return mainfunction()
+    
+    # add @ se não estiver presente
+    if inplist[1][0] != '@':
+        inplist[1] = f"@{inplist[1]}"
+
+    # grupo e sexo para maiúsculo
+    inplist[2] = inplist[2].upper()
+    inplist[3] = inplist[3].upper()
+    
+    # adicionando o ano à data se não estiver presente
+    i = 5
+    while i < 7:
+        if len(inplist[i]) < 8:
+            inplist[i] = inplist[i] + '/' + str(year)
+        i += 1
+
+    # inserindo na tabela pedidos
+    insert = f"INSERT INTO pedidos ('Nome','Instagram','Grupo','Sexo','Valor','Data Pedido','Data Entrega') VALUES ('{inplist[0]}', '{inplist[1]}', '{inplist[2]}', '{inplist[3]}', '{inplist[4]}', '{inplist[5]}', '{inplist[6]}')"
+    cursor.execute(insert)
+
+    # separando itens distintos e quantidades
+    itenspedidos = inplist[7].split(';')
+    i = 0
+    while i < len(itenspedidos):
+        itenspedidos[i] = itenspedidos[i].split('-')
+        print(itenspedidos)
+        print(itenspedidos[i][0], itenspedidos[i][1])
+        itenspedidos[i][0] = itenspedidos[i][0].strip()
+        itenspedidos[i][1] = itenspedidos[i][1].strip()
+        i += 1
+    
+    # selecionando colunas de acordo com itens
+    i = 0
+    colunas = ''
+    while i < len(itenspedidos):
+        if i < len(itenspedidos) - 1:
+            colunas += f"'{itenspedidos[i][1]}',"
+        else:
+            colunas += f"'{itenspedidos[i][1]}'"
+        i += 1
+
+    # selecionando quantidade de itens
+    i = 0
+    quantidades = ''
+    while i < len(itenspedidos):
+        if i < len(itenspedidos) - 1:
+            quantidades += itenspedidos[i][0] + ','
+        else:
+            quantidades += itenspedidos[i][0]
+        i += 1
+
+    # inserindo itens distintos e suas quantidades na tabela itenspedidos
+    insert = f"INSERT INTO itensPedidos ({colunas}) VALUES ({quantidades})"
+    cursor.execute(insert)
+
+    ### printando tabela
+    #cursor.execute("SELECT * FROM pedidos")
+    #results = cursor.fetchall()
+    #print(results)
+
+    #cursor.execute("SELECT * FROM itensPedidos")
+    #results = cursor.fetchall()
+    #print(results)
+
+    # commiting & restarting
+    commit()
+    mainfunction()
+    
+def commit():
+    print('commiting')
+    connection.commit()
+
+mainfunction()
